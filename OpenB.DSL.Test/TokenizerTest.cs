a80059ba48b6296cfcdcd29b0251350d611cb02f
wilko.vanderveen <wilko.vanderveen@gmail.com>
2015-01-20 14:22:30 +0100

    First implementation of simple tokenizer.

---
+++ OpenB.DSL.Test/TokenizerTest.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using NUnit.Framework;
+
+namespace OpenB.DSL.Test
+{
+    [TestFixture]
+    public class TokenizerTest
+    {
+        [Test]
+        public void SimpleTokenizer_TwoTokenTypes_GetCount_Returns_5()
+        {
+            var tokenizer = new Tokenizer(@"(?<expr>.+?\))((\s+(?<log_op>(AND|OR))(\s+?))|$)");
+            IList<Token> tokens = tokenizer.Tokenize("(1+1) AND (2+2) OR (3+3)").ToList();
+
+            Assert.That(tokens.Count, Is.EqualTo(5));
+        }
+
+        [Test]
+        public void ComplexTokenizer_TwoTokenTypes_FirstChildHasMatch()
+        {
+            var tokenizer = new Tokenizer(@"(?<expr>.+?\))((\s+(?<log_op>(AND|OR))(\s+?))|$)");
+
+            var innnerTokenizer = new Tokenizer(@"(\[(?<field>.+?)\])|(?<const>\d+(\.\d{0,5})?)|(?<operator>[\+|\-|\*])");
+            tokenizer.Add("expr", innnerTokenizer);
+
+            IList<Token> tokens = tokenizer.Tokenize("([Model.Property]+3.12) AND (2-1) OR (3*6)").ToList();
+            
+            Assert.That(tokens[0].ChildTokens.Count(), Is.EqualTo(3));
+            Assert.That(tokens[2].ChildTokens.Count() , Is.EqualTo(3));
+            Assert.That(tokens[4].ChildTokens.Count(), Is.EqualTo(3));
+        }
+    }
+}
--- OpenB.DSL/Tokenizer.cs
+++ OpenB.DSL/Tokenizer.cs
@@ -1,32 +1,71 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text.RegularExpressions;
 
 namespace OpenB.DSL
 {
     public class Tokenizer
     {
-        private readonly bool _recursive;
+        private readonly IDictionary<string, Tokenizer> _childTokenizers;
         private readonly Regex _regex;
 
-        public Tokenizer(string regularExpression, bool recursive)
+        public Tokenizer(string regularExpression)
         {
             _regex = new Regex(regularExpression, RegexOptions.Multiline);
-            _recursive = recursive;
+            _childTokenizers = new Dictionary<string, Tokenizer>();
+        }
+
+        public void Add(string groupName, Tokenizer tokenizer)
+        {
+            _childTokenizers.Add(groupName, tokenizer);
         }
 
         public IEnumerable<Token> Tokenize(string expression)
         {
-            throw new NotImplementedException();
+            IList<Token> tokens = new List<Token>();
 
             if (_regex.Match(expression).Success)
             {
-            }
-           
-        }
-    }
+                foreach (Match m in _regex.Matches(expression))
+                {
+                    foreach (string groupName in _regex.GetGroupNames())
+                    {
+                        int groupNumber;
 
-    public class Token
-    {
+                        if (int.TryParse(groupName, out groupNumber))
+                        {
+                            continue;
+                        }
+
+                        Group g = m.Groups[groupName];
+
+                        if (g.Success)
+                        {
+                            foreach (Capture capture in g.Captures)
+                            {
+                                bool canChildTokenizersBeApplied = _childTokenizers.Any(c => c.Key.Equals(groupName));
+
+                                Token token = canChildTokenizersBeApplied ? new Token(groupName, TokenizeChildren(groupName, capture.Value), capture.Value) : new Token(groupName, capture.Value);
+                                tokens.Add(token);
+                            }
+                        }
+                    }
+                }
+            }
+
+            return tokens;
+        }
+
+        private IEnumerable<Token> TokenizeChildren(string groupName, string value)
+        {
+            List<Token> tokens = new List<Token>();
+
+            foreach (Tokenizer tokenizer in _childTokenizers.Where(c => c.Key.Equals(groupName)).Select(c => c.Value))
+            {
+                tokens.AddRange(tokenizer.Tokenize(value));
+            }
+            return tokens;
+        }
     }
 }
